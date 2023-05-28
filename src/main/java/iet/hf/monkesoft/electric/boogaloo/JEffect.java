9c5ebff82e668979e9c6643d73c43e1726f2a04e
Xeldas <sipos1krisztian@gmail.com>
2023-05-28 17:37:55 +0200

    Source files moved from default package to iet.hf.monkesoft.electric.boogaloo
    Needed to be used with Cucumber

---
+++ src/main/java/iet/hf/monkesoft/electric/boogaloo/JEffect.java
@@ -0,0 +1,44 @@
+package iet.hf.monkesoft.electric.boogaloo;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+
+/**
+ * Az effekteket reprezentáló iet.hf.monkesoft.electric.boogaloo.JDrawable osztályok ősosztálya.
+ * */
+public class JEffect extends JDrawable {
+	
+	private AppliedEffect effect;
+	
+	public JEffect(String imgName, AppliedEffect e) {
+		super(imgName);
+		this.effect = e;
+	}
+	
+	/**
+	 * Kirajzolja az effekt ikonját es az idot
+	 * */
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.white);
+		if(effect.GetTime() > -1)
+		{
+			g.setColor(Color.red);
+			FontMetrics fm = g.getFontMetrics();
+            Rectangle2D rect = fm.getStringBounds(effect.GetTime() + "", g);
+            g.setColor(Color.black);
+            g.fillRect(4, 14 - fm.getAscent(),
+                       (int) rect.getWidth(),
+                       (int) rect.getHeight());
+            g.setColor(Color.white);
+			g.drawString(effect.GetTime() + "", 4, 14);
+		}
+		
+		if (effect.GetEffect().isIndividual()){
+			g.setColor(Color.blue);
+			Graphics2D g2 = (Graphics2D) g;
+			g2.setStroke(new BasicStroke(10));
+			g2.drawRect(0, 0, getWidth(), getHeight());
+		}
+	}
+}
---
+++ src/main/java/iet/hf/monkesoft/electric/boogaloo/JEqiupmentButton.java
@@ -0,0 +1,13 @@
+package iet.hf.monkesoft.electric.boogaloo;
+
+import javax.swing.JButton;
+
+public class JEqiupmentButton extends JButton {
+
+	int Id;
+	public JEqiupmentButton(String string, int id) {
+		super(string);
+		this.Id = id;
+	}
+
+}
